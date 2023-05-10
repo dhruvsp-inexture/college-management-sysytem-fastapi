313378b3116fdf51e1b118870791990da452a6db
DHRUV <dhruvsp.inexture@gmail.com>
2023-05-10 13:29:52 +0530

    Added stripe payment integration.

--- clg_man/Student/schemas.py
+++ clg_man/Student/schemas.py
@@ -1,5 +1,22 @@
+from typing import Union
 from pydantic import BaseModel
+
+
+class DropCourseRequestSchema(BaseModel):
+    course_id: int
 
 
 class EnrollCourseRequestSchema(BaseModel):
     course_id: int
+    payment_token: Union[str, None] = None
+
+
+class PaymentRequestSchema(BaseModel):
+    number: str
+    exp_month: int
+    exp_year: int
+    cvc: str
+
+    class Config:
+        orm_mode = True
+        extra = "forbid"
